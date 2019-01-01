86e54eadf4206ed90bec979b42ee4b0ec8e73c72
viktorsuharev <viktor.suhareff@yandex.ru>
2019-01-01 23:54:25 +0300

    add background img

--- app-ui/src/main/frontend/src/app/component/header/HeaderPanel.js
+++ app-ui/src/main/frontend/src/app/component/header/HeaderPanel.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./HeaderPanel.css";
 
 class HeaderPanel extends React.Component {
   render() {
     return (
       <div>
-        <h2>Happy New Year</h2>
+        <header className = "masthead">
+          <div className = "overlay"></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-md-10 mx-auto">
+                <div className="site-heading">
+                  <h1>Happy New Year</h1>
+                  <span className="subheading">Some text ...</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
